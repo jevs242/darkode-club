0ea471133386cb2b5b551bbf9aecc8fd269a47a8
Jevspr <jevelazquezs70@gmail.com>
2021-11-28 21:26:28 -0400

    CPP & Map

---
+++ README.md
@@ -0,0 +1,3 @@
+# MiniGameJam
+
+Developed with Unreal Engine 4
---
+++ Source/MiniGameJam/MiniGameJamGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MiniGameJamGameMode.h"
+#include "MiniGameJamHUD.h"
+#include "Player/CharacterBase.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMiniGameJamGameMode::AMiniGameJamGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprint/Player/BP_CharacterBase.BP_CharacterBase_C"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AMiniGameJamHUD::StaticClass();
+}
